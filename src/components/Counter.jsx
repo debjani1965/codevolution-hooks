b5f430d40cff11e278a54e0725595895a12fd6e2
Debjani Sen Gupta <debjanisg.sengupta@gmail.com>
2024-07-31 08:57:23 +0530

    Counter component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
+    return (
+       <button onClick={() => setCounter(counter + 1)}>Count {counter}</button>
+    )
+}
+
+export default Counter;
